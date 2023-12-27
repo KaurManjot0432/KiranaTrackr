cb766b040845b93c87bccde064ab8045b6400233
KaurManjot0432 <0432manjot@gmail.com>
2023-12-27 20:17:48 +0530

    added DTOs for Transactions

---
+++ src/main/java/com/example/KiranaTrackr/dtos/Transaction/TransactionRequestDTO.java
@@ -0,0 +1,21 @@
+package com.example.KiranaTrackr.dtos.Transaction;
+
+import com.example.KiranaTrackr.models.enums.CurrencyType;
+import com.example.KiranaTrackr.models.enums.PaymentType;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class TransactionRequestDTO {
+    private BigDecimal amount;
+    private PaymentType paymentType;
+    private CurrencyType currencyType;
+    private String customerId;
+    private String storeId;
+}
---
+++ src/main/java/com/example/KiranaTrackr/dtos/Transaction/TransactionResponseDTO.java
@@ -0,0 +1,24 @@
+package com.example.KiranaTrackr.dtos.Transaction;
+
+import com.example.KiranaTrackr.models.enums.CurrencyType;
+import com.example.KiranaTrackr.models.enums.PaymentType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class TransactionResponseDTO {
+    private String id;
+    private BigDecimal amount;
+    private PaymentType paymentType;
+    private CurrencyType currencyType;
+    private String customerId;
+    private String storeId;
+    private LocalDateTime timestamp;
+}
